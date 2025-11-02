81640fdc8edcc708f4f5ca35f535cf78932b53d5
Bernd Herding <bernd.herding@external.biontech.com>
2025-11-02 21:48:51 +0100

    delete everything but workflows

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "backend_bucket" {
-  description = "Central backend bucket of the Storm Library for Terraform (SLT)™"
-  type        = string
-}
-
-variable "oidc_principal" {
-  description = "Github owner (org or user) of repositories permitted to deploy to AWS"
-  type        = string
-}
