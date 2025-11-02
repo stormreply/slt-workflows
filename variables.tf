eeca1e745a6beddc7ec160f8d2513398b8283a5a
Bernd Herding <berndherding@users.noreply.github.com>
2025-11-02 21:34:22 +0100

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "backend_bucket" {
+  description = "Central backend bucket of the Storm Library for Terraform (SLT)™"
+  type        = string
+}
+
+variable "oidc_principal" {
+  description = "Github owner (org or user) of repositories permitted to deploy to AWS"
+  type        = string
+}
